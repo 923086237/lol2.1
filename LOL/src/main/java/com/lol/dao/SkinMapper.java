2684fad98e8818006361b25c150deb4593c96902
923086237 <2817484971@qq.com>
2018-10-17 22:07:18 +0800

    2.3版本 注册 登录 英雄展示和分页 皮肤Service之前

---
+++ LOL/src/main/java/com/lol/dao/SkinMapper.java
@@ -0,0 +1,22 @@
+package com.lol.dao;
+
+import com.lol.domain.Skin;
+
+import java.util.List;
+import java.util.Map;
+
+public interface SkinMapper {
+    /**
+     * 查询所有皮肤
+     * @return 皮肤列表
+     */
+    public List<Skin> findAllSkin();
+
+    /**
+     * 分页
+     * @param info
+     * @return
+     */
+    public List<Skin> findAllSkinLimit(Map<String, Object> info);
+
+}
